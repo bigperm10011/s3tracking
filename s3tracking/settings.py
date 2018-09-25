f79613c771a35c9e10ce5dea30072c2828e8c071
bigperm10011 <22182357+bigperm10011@users.noreply.github.com>
2018-09-25 16:39:33 -0400

    Update settings.py

--- s3tracking/settings.py
+++ s3tracking/settings.py
@@ -28,7 +28,7 @@ ITEM_PIPELINES = {
     's3tracking.pipelines.S3TrackingPipeline': 300,
 }
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 's3tracking (+http://www.yourdomain.com)'
+USER_AGENT = USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 
 # Obey robots.txt rules
 #ROBOTSTXT_OBEY = True
